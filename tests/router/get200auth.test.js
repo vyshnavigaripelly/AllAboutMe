cdf6c8c7bd00109d306c6c9ffd9790c9c75f3215
vyshnavigaripelly <41191430+vyshnavigaripelly@users.noreply.github.com>
2020-03-05 23:16:59 -0600

    Update get200auth.test.js

--- tests/router/get200auth.test.js
+++ tests/router/get200auth.test.js
@@ -1,4 +1,4 @@
-const test = require('tape');.
+const test = require('tape');
 const request = require('supertest');
 const app = require('../../src/app');
 const dbReset = require('../../src/model/database/db_seed');
