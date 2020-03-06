9c7b562288eb085f2509b49e31868267ee99545c
vyshnavigaripelly <41191430+vyshnavigaripelly@users.noreply.github.com>
2020-03-05 23:31:00 -0600

    Update user_queries.js

--- src/model/user_queries.js
+++ src/model/user_queries.js
@@ -1,4 +1,4 @@
-const dbConnection = require('./database/db_connection');.
+const dbConnection = require('./database/db_connection');
 
 const queries = {
   insertUser: 'INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING id',
