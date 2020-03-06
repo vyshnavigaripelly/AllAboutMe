91a9dfb6e915a404851f4e917ef3df0b30cc9de1
vyshnavigaripelly <41191430+vyshnavigaripelly@users.noreply.github.com>
2020-03-05 23:31:41 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-const express = require('express');.
+const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const cookieSession = require('cookie-session');
